9407c39791db590432e9f471106a65a0a4bf4efb
Foxlora <18351937137@163.com>
2020-11-17 14:22:50 +0800

    添加数据集

--- src/learn_groupby.py
+++ src/learn_groupby.py
@@ -8,8 +8,16 @@ __time__ = '2020/10/13 12:41'
 
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 df = pd.read_csv('../data/James_Harden.csv',encoding='utf8')
 
-grouped = df.groupby(['胜负'])
+grouped = df.groupby(['胜负']).count()
 
-print(grouped.count())
+colorMap = {value:index for index,value in enumerate(set(df["胜负"]))}
+df['胜负'] = df['胜负'].map(colorMap )
+
+
+b = tf.ones((2,3)) * 3
+c = tf.pow(b,2)
+print(b)
+print(c)
